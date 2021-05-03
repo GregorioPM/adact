65aa1d7c1806ae96a16c296c77d1f83eea1f127f
GregorioPM <josegregoriopm@ufps.edu.co>
2021-05-03 14:59:26 -0500

    Errores en la ruta

--- libs/app.php
+++ libs/app.php
@@ -1,58 +1,66 @@
 <?php
-Class App{
-    
-    function __construct(){
-        $url = isset($_GET['url']) ? $_GET['url'] : null;
-        $url = rtrim($url, '/');
-        $url = explode('/', $url);
+require_once 'controllers/errores.php';
 
-         //var_dump($url);
+class App{
+
+    function __construct(){
+
+        $url = isset($_GET['url']) ? $_GET['url']: null;
+        $url = rtrim($url, '/');
+        //var_dump($url);
         /*
             controlador->[0]
             metodo->[1]
             parameter->[2]
         */
+        $url = explode('/', $url);
 
+        // cuando se ingresa sin definir controlador
         if(empty($url[0])){
-            error_log('APP::contruct->no hay controlador');
             $archivoController = 'controllers/login.php';
             require_once $archivoController;
-            $controller = new Login(); 
+            $controller = new Login();
             $controller->loadModel('login');
             $controller->render();
             return false;
         }
-        $archivoController = 'controllers/' . url[0] . '.php';
+        $archivoController = 'controllers/' . $url[0] . '.php';
+
         if(file_exists($archivoController)){
             require_once $archivoController;
 
+            // inicializar controlador
             $controller = new $url[0];
             $controller->loadModel($url[0]);
 
+            // si hay un método que se requiere cargar
             if(isset($url[1])){
                 if(method_exists($controller, $url[1])){
                     if(isset($url[2])){
-                        $nparam = count($url)-2;
-                        $params= [];
-                        for ($i=0; $i < $nparam; $i++) { 
-                            array_push($params, $url[$i]+2);
+                        //el método tiene parámetros
+                        //sacamos e # de parametros
+                        $nparam = sizeof($url) - 2;
+                        //crear un arreglo con los parametros
+                        $params = [];
+                        //iterar
+                        for($i = 0; $i < $nparam; $i++){
+                            array_push($params, $url[$i + 2]);
                         }
+                        //pasarlos al metodo   
                         $controller->{$url[1]}($params);
                     }else{
-                        //no tiene parametros se llama el metodo tal cual
-                        $controller->{$url[1]}();
+                        $controller->{$url[1]}();    
                     }
                 }else{
-                    //error, no existe el metodo
+                    $controller = new Errores(); 
                 }
             }else{
-                //no hay metodo a cargar, se carga el metodo por default
                 $controller->render();
             }
         }else{
-            //no existe el archivo, mandar error
-        }    
+            $controller = new Errores();
+        }
     }
-
 }
+
 ?>

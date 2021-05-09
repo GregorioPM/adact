5df4de786389fd58d16f9a250ca963cb3e808361
GregorioPM <josegregoriopm@ufps.edu.co>
2021-05-09 15:36:46 -0500

    logout y eliminar y listar dependencias

--- views/admin/index.php
+++ views/admin/index.php
@@ -8,10 +8,13 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 <body>
+
+<?php require_once 'header.php';?>
 <div id="main-container">
 
 
     <h1>admin logeado</h1>
+    <a href="<?= URL ?>/admin/getDependencias">Dependecias</a>
     <button class="btn-main" id="new-dependencia">
                             <i class="material-icons">add</i>
                             <span>Registrar nueva Dependencia</span>
